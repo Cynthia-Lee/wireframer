57f2974a2b0a642de68124a1b4afe07a2170662b
Cynthia Lee <cynthia.lee335@gmail.com>
2019-12-12 02:54:38 -0500

    Changed Firebase config

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -5,14 +5,14 @@ import 'firebase/auth';
 // THIS IS USED TO INITIALIZE THE firebase OBJECT
 // PUT YOUR FIREBASE PROJECT CONFIG STUFF HERE
 var firebaseConfig = {
-    apiKey: "AIzaSyB651F8jJUazEWTJ2666ZDzBr-yw8pfsHo",
-    authDomain: "todo-rrf-316-c.firebaseapp.com",
-    databaseURL: "https://todo-rrf-316-c.firebaseio.com",
-    projectId: "todo-rrf-316-c",
-    storageBucket: "todo-rrf-316-c.appspot.com",
-    messagingSenderId: "463939744359",
-    appId: "1:463939744359:web:56a4f7a5c5fe37e58d5ac2",
-    measurementId: "G-658NWVT68B"
+    apiKey: "AIzaSyBEFTj9rN4k8dt6GlqXC75icwhKrIBWYjs",
+    authDomain: "wireframer-cyllee.firebaseapp.com",
+    databaseURL: "https://wireframer-cyllee.firebaseio.com",
+    projectId: "wireframer-cyllee",
+    storageBucket: "wireframer-cyllee.appspot.com",
+    messagingSenderId: "222641140248",
+    appId: "1:222641140248:web:107b4738cab22ce2fc496d",
+    measurementId: "G-Y7182R21F9"
 
     // My Firebase
     /*
@@ -24,18 +24,6 @@ var firebaseConfig = {
     messagingSenderId: "310472154444",
     appId: "1:310472154444:web:3c6b5bf103de9f034a03e9",
     measurementId: "G-V56WNREQQW"
-    */
-
-    // McKenna's Firebase
-    /*
-    apiKey: "AIzaSyCJxkqx-6PMJrZ7ACkrgbO55b5wmJdop1Y",
-    authDomain: "todo-rrf-316.firebaseapp.com",
-    databaseURL: "https://todo-rrf-316.firebaseio.com",
-    projectId: "todo-rrf-316",
-    storageBucket: "todo-rrf-316.appspot.com",
-    messagingSenderId: "892398996038",
-    appId: "1:892398996038:web:1fb9157fc6c5d266e01847",
-    measurementId: "G-TEGQB3MZ23"
     */
 };
 firebase.initializeApp(firebaseConfig);
