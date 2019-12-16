db6b07d356ee8da16c89f9598c1c48f046d20dfe
Cynthia Lee <cynthia.lee335@gmail.com>
2019-12-15 21:35:37 -0500

    Admins have access to test database

--- src/test/DatabaseTester.js
+++ src/test/DatabaseTester.js
@@ -18,8 +18,6 @@ class DatabaseTester extends React.Component {
                 fireStore.collection('wireframeList').doc(doc.id).delete();
             })
         });
-
-        console.log(this.props.users);
     }
 
     handleReset = () => {
@@ -42,12 +40,50 @@ class DatabaseTester extends React.Component {
         });
     }
 
+    getAdmins = () => {
+        if (this.props.users) {
+            let people = this.props.users;
+            var i;
+            // console.log(this.props.users);
+            // console.log(this.props.auth.uid); // find in users
+            var i;
+            var admins = [];
+            for (i = 0; i < people.length; i++) {
+                if (people[i]["admin"] == 1) {
+                    admins.push(people[i]);
+                    // console.log(person);
+                }
+            }
+            // console.log(admins);
+            return admins;
+        }
+    }
+
+    checkAdmin() {
+        let admins = this.getAdmins();
+        if (this.props.auth && admins) {
+            var i;
+            for (i = 0; i < admins.length; i++) {
+                if (this.props.auth.uid == admins[i]["id"]) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+
     render() {
-        return (
-            <div>
-                <button onClick={this.handleClear}>Clear Database</button>
-                <button onClick={this.handleReset}>Reset Database</button>
-            </div>)
+        if (this.props.auth && this.checkAdmin()) { // user is an admin
+            // console.log(this.checkAdmin());
+            return (
+                <div>
+                    <button onClick={this.handleClear}>Clear Database</button>
+                    <button onClick={this.handleReset}>Reset Database</button>
+                </div>
+            )
+        } else {
+            return <div></div>
+        }
     }
 }
 
@@ -55,8 +91,13 @@ const mapStateToProps = function (state) {
     return {
         auth: state.firebase.auth,
         firebase: state.firebase,
-        user: state.firebase.ordered.users
+        users: state.firestore.ordered.users
     };
 }
 
-export default compose(connect(mapStateToProps), firestoreConnect([{collection: "users"}]))(DatabaseTester);
+export default compose(
+    connect(mapStateToProps), 
+    firestoreConnect([
+        {collection: "users"}
+    ]),
+)(DatabaseTester);
